2e5120d6152c02ce896fd2cea337184a9b3abeb9
KubaBoi <jakubanderle@outlook.cz>
2022-12-15 19:50:17 -0600

    delani kalulacky

--- examples/alloc.s
+++ examples/alloc.s
@@ -1,16 +1,38 @@
-section .text
-   global _start          ;must be declared for linker (gcc)
+section	.text
+   global _start         ;must be declared for using gcc
 	
-_start:                   ;tell linker entry point
-   mov	edx,count	  ;message length
-   mov	ecx,marks        ;message to write
-   mov	ebx,1		  ;file descriptor (stdout)
-   mov	eax,4		  ;system call number (sys_write)
-   int	0x80		  ;call kernel
+_start:	                 ;tell linker entry point
 
-   mov	eax,1		  ;system call number (sys_exit)
-   int	0x80		  ;call kernel
+   mov	eax, 45		 ;sys_brk
+   xor	ebx, ebx
+   int	80h
 
-section .data
-count EQU 9
-marks TIMES count DB '*'
+   add	eax, 16384	 ;number of bytes to be reserved
+   mov	ebx, eax
+   mov	eax, 45		 ;sys_brk
+   int	80h
+	
+   cmp	eax, 0
+   jl	exit	;exit, if error 
+   mov	edi, eax	 ;EDI = highest available address
+   sub	edi, 4		 ;pointing to the last DWORD  
+   mov	ecx, 4096	 ;number of DWORDs allocated
+   xor	eax, eax	 ;clear eax
+   std			 ;backward
+   rep	stosd            ;repete for entire allocated area
+   cld			 ;put DF flag to normal state
+	
+   mov	eax, 4
+   mov	ebx, 1
+   mov	ecx, msg
+   mov	edx, len
+   int	80h		 ;print a message
+
+exit:
+   mov	eax, 1
+   xor	ebx, ebx
+   int	80h
+	
+section	.data
+msg    	db	"Allocated 16 kb of memory!", 10
+len     equ	$ - msg
--- test.s
+++ test.s
@@ -1,131 +1,64 @@
-; File Handling System Calls
-; %eax      %name       %ebx            %ecx        %edx
-; 2         sys_fork    struct pt_regs  -           -
-; 3         sys_read    uint            char*       size_t
-; 4         sys_write   uint            const char* size_t
-; 5         sys_open    const char*     int         int
-; 6         sys_close   uint            -           -
-; 8         sys_creat   const char*     int         -
-; 19        sys_lseek   uint            off_t       uint
+%macro print 2
+mov	eax, 4
+mov	ebx, 1
+mov	ecx, %1
+mov	edx, %2
+int	80h		 ;print a message
+%endmacro
 
-;CREATE
-;    Put the system call sys_creat() number 8, in the EAX register.
-;    Put the filename in the EBX register.
-;    Put the file permissions in the ECX register.
-; 
-; The system call returns the file descriptor of the created file
-; in the EAX register, in case of error, 
-; the error code is in the EAX register.
+%macro read 2
+mov eax, 3
+mov ebx, 2
+mov ecx, %1 
+mov edx, %2          ;5 bytes (numeric, 1 for sign) of that information
+int 80h
+%endmacro
 
-;OPEN
-;    Put the system call sys_open() number 5, in the EAX register.
-;    Put the filename in the EBX register.
-;    Put the file access mode in the ECX register.
-;    Put the file permissions in the EDX register.
-; 
-; Among the file access modes, most commonly used are:
-;  read-only (0), write-only (1), and read-write (2).
-
-;READ
-;    Put the system call sys_read() number 3, in the EAX register.
-;    Put the file descriptor in the EBX register.
-;    Put the pointer to the input buffer in the ECX register.
-;    Put the buffer size, i.e., the number of bytes to read, 
-;           in the EDX register.
-
-;CLOSE
-;    Put the system call sys_close() number 6, in the EAX register.
-;    Put the file descriptor in the EBX register.
-
-;UPDATE
-;    Put the system call sys_lseek () number 19, in the EAX register.
-;    Put the file descriptor in the EBX register.
-;    Put the offset value in the ECX register.
-;    Put the reference position for the offset in the EDX register.
-;
-; The reference position could be:
-;    Beginning of file - value 0
-;    Current position - value 1
-;    End of file - value 2
+%macro isNumber 1
+cmp [%1], byte ":"
+jge error
+cmp [%1], byte "0"
+jl error
+%endmacro
 
 section	.text
    global _start         ;must be declared for using gcc
 	
-_start:                  ;tell linker entry point
-   ;create the file
-   mov  eax, 8
-   mov  ebx, file_name
-   mov  ecx, 0777        ;read, write and execute by all
-   int  0x80             ;call kernel
-	
-   mov [fd_out], eax
-    
-   ; write into the file
-   mov	edx,len          ;number of bytes
-   mov	ecx, msg         ;message to write
-   mov	ebx, [fd_out]    ;file descriptor 
-   mov	eax,4            ;system call number (sys_write)
-   int	0x80             ;call kernel
-	
-   ; close the file
-   mov eax, 6
-   mov ebx, [fd_out]
-    
-   ; write the message indicating end of file write
-   mov eax, 4
-   mov ebx, 1
-   mov ecx, msg_done
-   mov edx, len_done
-   int  0x80
-    
-   ;open the file for reading
-   mov eax, 5
-   mov ebx, file_name
-   mov ecx, 0             ;for read only access
-   mov edx, 0777          ;read, write and execute by all
-   int  0x80
-	
-   mov  [fd_in], eax ; file descriptor
-    
-   ;read from file
-   mov eax, 3
-   mov ebx, [fd_in]
-   mov ecx, info
-   mov edx, 26
-   int 0x80
-    
-   ; close the file
-   mov eax, 6
-   mov ebx, [fd_in]
-   int  0x80    
-	
-   ; print the info 
-   mov eax, 4
-   mov ebx, 1
-   mov ecx, info
-   mov edx, 26
-   int 0x80
-       
-   mov	eax,1             ;system call number (sys_exit)
-   int	0x80              ;call kernel
+_start:	                 ;tell linker entry point
+    print msg1, len1
+    read num1, 5
+    isNumber num1
 
+    print msg1, len1
+    read num2, 5
+    isNumber num2
+
+    mov al, [num1]
+    sub al, "0"
+    add al, [num2]
+
+    mov [sum], al
+
+    print sum, 1
+
+    jmp exit
+
+error:
+    print msg2, len2
+
+exit:
+    mov	eax, 1
+    int	80h
+	
 section	.data
-file_name db 'myfile.txt', 0 ; bacha na koncovy nuly
-msg db 'Welcome to Tutorials Point', 0
-len equ  $-msg
+    msg1 db "Enter number:", 0xa
+    len1 equ $ - msg1
 
-msg_done db 'Written to file', 0xa
-len_done equ $-msg_done
+    msg2 db "Need to be number.", 0xa
+    len2 equ $ - msg2
+
+    sum db " ", 0
 
 section .bss
-fd_out resb 1
-fd_in  resb 1
-info resb  26
-
-
-
-
-
-
-
-
+    num1 resb 5
+    num2 resb 5
