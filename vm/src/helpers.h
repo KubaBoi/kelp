2bc193a2667424fc13aa0de6f18de79345f5380f
KubaBoi <jakubanderle@outlook.cz>
2023-05-09 23:03:08 +0200

    podminky

--- vm/src/helpers.h
+++ vm/src/helpers.h
@@ -11,4 +11,6 @@ void printBits(size_t const size, void const *const ptr);
 
 void toCharArray(size_t const size, ptr_t ptr, byte_t *trg);
 
+void moduloByTwo(byte_t num, byte_t *modulo, byte_t *div);
+
 #endif
